e4395e69aaae9710df934073e952b327d987a34a
SuzukiHonoka <1787074172@qq.com>
2020-05-15 20:12:35 +0800

    Always uses the end of filename for testing.

--- main.go
+++ main.go
@@ -11,11 +11,12 @@ import (
 
 const pfn = "main.go"
 const ncmd = true
-var valid = []string{"MP3","FLAC","WAV"}
+
+var valid = []string{"MP3", "FLAC", "WAV"}
 
 func main() {
 	var args = os.Args
-	var dir,_ = os.Getwd()
+	var dir, _ = os.Getwd()
 	var spath = "."
 	if len(args) > 1 {
 		spath = args[1]
@@ -23,21 +24,21 @@ func main() {
 	if spath == "." {
 		spath = dir
 	}
-	file,_ := ioutil.ReadDir(dir)
-	for _,f := range file {
+	file, _ := ioutil.ReadDir(dir)
+	for _, f := range file {
 		if !f.IsDir() {
 			name := f.Name()
 			if name != pfn {
 				fext := filepath.Ext(name)
 				ext := ""
-				if len(fext)!=0 && len(fext) <= 5 {
+				if len(fext) != 0 && len(fext) <= 5 {
 					ext = strings.ToUpper(fext[1:])
 				} else {
 					// for ncm dump meta error
 					if ncmd {
-						ins,ffext := includef(name)
+						ins, ffext := includef(name)
 						if ins {
-							fmt.Println("Broken NCM:",name)
+							fmt.Println("Broken NCM:", name)
 							ext = ffext
 						} else {
 							continue
@@ -47,11 +48,11 @@ func main() {
 						continue
 					}
 				}
-				abss := filepath.Join(dir,name)
-				absd := filepath.Join(dir,ext)
-				status,err := movefile(abss,absd)
+				abss := filepath.Join(dir, name)
+				absd := filepath.Join(dir, ext)
+				status, err := movefile(abss, absd)
 				if status {
-					fmt.Println("Move:",abss,absd,"Succeed.")
+					fmt.Println("Move:", abss, absd, "Succeed.")
 				} else {
 					fmt.Println(err.Error())
 				}
@@ -60,33 +61,38 @@ func main() {
 	}
 }
 
-func movefile(src string,dst string)(bool,error) {
+func movefile(src string, dst string) (bool, error) {
 	if !exists(dst) {
-		err := os.Mkdir(dst,os.ModePerm)
-		if err != nil{
-			return false,err
+		err := os.Mkdir(dst, os.ModePerm)
+		if err != nil {
+			return false, err
 		}
 	}
-	err := os.Rename(src,filepath.Join(dst,path.Base(src)))
-	if err != nil{
+	err := os.Rename(src, filepath.Join(dst, path.Base(src)))
+	if err != nil {
 		return false, err
 	}
-	return true,nil
+	return true, nil
 }
 
-func exists(path string) (bool) {
+func exists(path string) bool {
 	_, err := os.Stat(path)
-	if err == nil { return true }
-	if os.IsNotExist(err) { return false }
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
 	return false
 }
 
-func includef(name string)(bool,string){
+func includef(name string) (bool, string) {
 	bn := strings.ToUpper(name)
-	for _,f := range valid {
-		if strings.Contains(bn,f) {
-			return true,f
+	for _, f := range valid {
+		li := strings.LastIndex(bn, f)
+		if len(name[li+len(f)-1:]) == 0 {
+			return true, f
 		}
 	}
-	return false,""
+	return false, ""
 }
